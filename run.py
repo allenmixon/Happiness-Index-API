981c770abcaefab1778fd0208dd467b26a66f487
Allen Mixon <johnamixon@gmail.com>
2020-10-30 14:58:48 -0700

    Return index by id is working

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
+import json
 
 app = Flask(__name__)
+
+#load data from file
+with open('happiness-index-seed-data.json') as json_file:
+    json_data = json.load(json_file)
+
+#print (json_data)
+
+@app.route('/happiness-index', methods=['GET'])
+def get_all():
+	return jsonify(json_data)
+
+@app.route('/happiness-index/<id>', methods=['GET'])
+def get_county(id):
+	value = json_data.get(id)
+
+	if(value):
+		return jsonify({id:value})
+	else:
+		abort(404, description="Resource not found. That county does not exist.")
 
 if __name__ == "__main__":
 	app.run(debug=True)
