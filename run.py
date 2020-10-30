7c77616f3aa262c6c121b9871f4ade23f31edbfe
Allen Mixon <johnamixon@gmail.com>
2020-10-30 15:24:24 -0700

    JSON 404 response

--- run.py
+++ run.py
@@ -7,12 +7,17 @@ app = Flask(__name__)
 with open('happiness-index-seed-data.json') as json_file:
     json_data = json.load(json_file)
 
-#print (json_data)
+#return json 404
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
+#return all data
 @app.route('/happiness-index', methods=['GET'])
 def get_all():
 	return jsonify(json_data)
 
+#return individual county
 @app.route('/happiness-index/<id>', methods=['GET'])
 def get_county(id):
 	value = json_data.get(id)
